36f7f0674f1c08bcd6555826f548647971b056d1
Armia <armiaebrahimalam@gmail.com>
2021-12-21 23:22:52 +0800

    test

--- django_project/settings.py
+++ django_project/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'exhlfdat&vfum(-34*c2uroi(($ww(yo$9pv98=e6p^gl(-eoj'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['armia-myblog.herokuapp.com', '127.0.0.1']
+ALLOWED_HOSTS = ['baiarmiaebrahim.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
